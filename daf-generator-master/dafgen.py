da423db9887209aa3d8447280dc6c9022e611f73
Suna Gülmez <109751575+sunaglmez@users.noreply.github.com>
2024-06-23 17:04:22 +0300

    Update dafgen.py

--- daf-generator-master/dafgen.py
+++ daf-generator-master/dafgen.py
@@ -1,31 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-@file dafgen.py
-@author Sam Freeside <snovvcrash@protonmail[.]ch>
-@date 2018-01
-
-@brief Simple DAF (Delayed Auditory Feedback) Generator
-
-@license
-Copyright (C) 2018 Sam Freeside
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-@endlicense
-"""
-
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from ui_dafgen import Ui_DAFGen
